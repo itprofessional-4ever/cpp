286cfd961f834fd32c778d7aa114511bd14c70c7
Chloe <itprofessional.4ever7@gmail.com>
2024-09-10 15:18:43 -0600

    example for string type for searching an array

--- searchAnArrayForAnElement.cpp
+++ searchAnArrayForAnElement.cpp
@@ -1,29 +1,35 @@
 #include<iostream>
 
-int searchArray(int array[], int size, int element);
+int searchArray(std::string array[], int size, std::string element);
 
 int main() {
-  int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  int size = sizeof(numbers) / sizeof(numbers[0]);
+  // int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  std::string foods[] = {"pizza", "hamburger", "hotdog"};
+  // int size = sizeof(numbers) / sizeof(numbers[0]);
+  int size = sizeof(foods) / sizeof(foods[0]);
   int index;
-  int myNum;
+  // int myNum;
+  std::string myFood;
 
-  std::cout << "Enter elements to search for: " << '\n';
-  std::cin >> myNum;
+  // std::cout << "Enter elements to search for: " << '\n';
+  // std::cin >> myNum;
 
-  index = searchArray(numbers, size, myNum);
+  std::cout << "Enter Food to search for: " << '\n';
+  std::getline(std::cin, myFood);
+
+  index = searchArray(foods, size, myFood);
 
 if(index != -1) {
-  std::cout << myNum << " is at index " << index;
+  std::cout << myFood << " is at index " << index;
 }
 else {
-  std::cout << myNum << " is not in the array ";
+  std::cout << myFood << " is not in the array ";
 }
 
   return 0;
 }
 
-int searchArray(int array[], int size, int element) {
+int searchArray(std::string array[], int size, std::string element) {
   
   for(int i = 0; i < size; i++) {
     if(array[i] == element) {
