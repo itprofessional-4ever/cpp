43374b6ecdae9660e7ec2877fd962a222d60e612
Chloe <itprofessional.4ever7@gmail.com>
2024-09-10 15:52:46 -0600

    Fill Function

---
+++ FillFunction.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+  // fill() = Fills a range of elements with a specified value
+  // fill(begin, end, value)
+
+  // std::string foods[10] = {
+  //   "pizza", "pizza", "pizza", "pizza", "pizza", "pizza", "pizza", "pizza", "pizza", "pizza"};
+
+  const int SIZE = 100;
+  std::string foods[SIZE];
+
+  fill(foods, foods + SIZE, "pizza");
+
+  for (std::string food:foods) {
+    std::cout << food << '\n';
+  }
+
+  return 0;
+}
