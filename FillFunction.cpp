53823adfc9e01b5868360f56e1b698e02811cde7
Chloe <itprofessional.4ever7@gmail.com>
2024-09-10 16:21:56 -0600

    Fill an arrau with user input

--- FillFunction.cpp
+++ FillFunction.cpp
@@ -7,11 +7,13 @@ int main() {
   // std::string foods[10] = {
   //   "pizza", "pizza", "pizza", "pizza", "pizza", "pizza", "pizza", "pizza", "pizza", "pizza"};
 
-  const int SIZE = 100;
+  const int SIZE = 99;
   std::string foods[SIZE];
 
-  fill(foods, foods + (SIZE / 2), "pizza"); // first half of the array
-  fill(foods + (SIZE / 2), foods + SIZE, "hamburger"); // the last of the array
+  fill(foods, foods + (SIZE / 3), "pizza"); // first one third of the array
+  fill(foods + (SIZE / 3), foods + (SIZE/3) * 2, "hamburger"); // the second one third of the array
+  fill(foods + (SIZE/3) * 2, foods + SIZE, "hotdog"); // the last of the array
+
 
   for (std::string food:foods) {
     std::cout << food << '\n';
