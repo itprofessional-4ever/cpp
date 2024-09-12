e04a13941ca1307abe1ad0c6ed23d2a2270a11a8
Chloe <itprofessional.4ever7@gmail.com>
2024-09-12 14:29:01 -0600

    add

--- multiple Dimension.cpp
+++ multiple Dimension.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
 int main() {
-  std::string cars[][3] = {{"Mustang", "Escape", "F-150"},
+  std::string cars[][3] = {
+                          {"Mustang", "Escape", "F-150"},
                           {"Corvette", "Equinox", "Silverado"},
-                          {"Challenger", "Durango", "Ram 1500"}};
-  
+                          {"Challenger", "Durango", "Ram 1500"}
+                          };
+
+  std::cout << cars[0][0] << " ";
   return 0;
 }
