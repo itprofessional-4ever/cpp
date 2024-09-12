a99a0a432109a7e270ab9afa8a65ac260ab39131
Chloe <itprofessional.4ever7@gmail.com>
2024-09-12 13:45:39 -0600

    uer input

---
+++ multiple Dimension.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  std::string cars[][3] = {{"Mustang", "Escape", "F-150"},
+                          {"Corvette", "Equinox", "Silverado"},
+                          {"Challenger", "Durango", "Ram 1500"}};
+  
+  return 0;
+}
